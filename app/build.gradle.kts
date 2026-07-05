ab8e58707fa3142a49dff6806fa1a35bcee3f568
Nawar Ali <111868983+NawarAli1912@users.noreply.github.com>
2026-07-05 22:10:56 +0200

    bump-version-to-1.0.0-beta2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.daftar.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.0-beta1"
+        versionCode = 3
+        versionName = "1.0.0-beta2"
     }
 
     buildFeatures {

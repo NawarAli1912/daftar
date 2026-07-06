0ee11ac09b8ba392640faa480a9e93dbdd25f076
Nawar Ali <111868983+NawarAli1912@users.noreply.github.com>
2026-07-06 18:25:55 +0200

    bump-version-to-1.0.0-rc7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.daftar.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.0.0-rc6"
+        versionCode = 10
+        versionName = "1.0.0-rc7"
     }
 
     buildFeatures {

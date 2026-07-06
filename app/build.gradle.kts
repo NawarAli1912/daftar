f31b4df0b9220ec5b82d563735155ab3f4d0d312
Nawar Ali <111868983+NawarAli1912@users.noreply.github.com>
2026-07-06 14:14:02 +0200

    bump-version-to-1.0.0-rc4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.daftar.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.0-rc3"
+        versionCode = 7
+        versionName = "1.0.0-rc4"
     }
 
     buildFeatures {

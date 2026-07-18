402b692735222680d54ec9d1d4b4ec531a531de0
Nawar Ali <111868983+NawarAli1912@users.noreply.github.com>
2026-07-18 19:09:21 +0200

    release-v1.0.0-first-major-release-version-1.0.0-code-13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.daftar.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.0.0-rc9"
+        versionCode = 13
+        versionName = "1.0.0"
     }
 
     buildFeatures {
